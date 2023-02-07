2c7bd7b74828385f3d8bec33064ea132b01c50d7
wildanzr <graita.sukma@gmail.com>
2023-02-07 13:37:03 +0700

    feat: dockerization

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 // Conifg environment
 require('dotenv').config()
 
-// Database
-const mongoose = require('mongoose')
+// Wait for RabbitMQ to start before starting the consumer, set timeout for 30 seconds
+setTimeout(() => {
+  // Database
+  const mongoose = require('mongoose')
 
-// Service
-const { SubmissionService, ProblemSubmissionService, Consumer, Worker } = require('./services')
-const submissionService = new SubmissionService()
-const problemSubmissionService = new ProblemSubmissionService()
-const worker = new Worker(submissionService, problemSubmissionService)
-const consumer = new Consumer(submissionService, problemSubmissionService, worker)
+  // Service
+  const { SubmissionService, ProblemSubmissionService, Consumer, Worker } = require('./services')
+  const submissionService = new SubmissionService()
+  const problemSubmissionService = new ProblemSubmissionService()
+  const worker = new Worker(submissionService, problemSubmissionService)
+  const consumer = new Consumer(submissionService, problemSubmissionService, worker)
 
-// Connect to mongodb
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewURLParser: true,
-  useUnifiedTopology: true
-}).then(console.log('connected to db')).catch((err) => console.log(err))
+  // Connect to mongodb
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+  }).then(console.log('connected to db')).catch((err) => console.log(err))
 
-// Start consumer
-consumer.consumeMessage()
+  // Start consumer
+  consumer.consumeMessage()
+}, 30000)
+
+console.log('Waiting for RabbitMQ to start...')
